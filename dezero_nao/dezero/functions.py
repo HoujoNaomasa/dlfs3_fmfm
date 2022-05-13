6939bb58717ded9fcf642686fb8cf9e088084b9c
Houjoe <n-fujino-4r6@eagle.sophia.ac.jp>
2022-05-14 00:13:29 +0900

    step54.py

--- dezero_nao/dezero/functions.py
+++ dezero_nao/dezero/functions.py
@@ -468,3 +468,15 @@ def binary_cross_entropy(p, t):
     y = -1 * sum(tlog_p) / N
     return y
 
+
+def dropout(x, dropout_ratio=0.5):
+    x = as_variable(x)
+
+    if dezero.Config.train:
+        xp = cuda.get_array_module(x)
+        mask = xp.random.rand(*x.shape) > dropout_ratio
+        scale = xp.array(1.0 - dropout_ratio).astype(x.dtype)
+        y = x * mask / scale
+        return y
+    else:
+        return x
